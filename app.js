fc5c43092e600c5ed317ab4c6e671c14f127eb01
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-08-18 17:21:09 +0400

    CAPTIAL P

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ server.get("/removeAllMongo", function (req, res) {
 
     player.find(function (err, Player) {
         if (err) return console.error(err);
-        player.deleteMany({});
+        Player.deleteMany({});
     });
 });
    
