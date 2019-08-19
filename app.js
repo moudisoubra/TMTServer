a9b9187bfa94773752579f02fab503387450044b
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-08-19 13:11:18 +0400

    stringify

--- app.js
+++ app.js
@@ -109,8 +109,9 @@ server.get("/listAllMongo", function (req, res) { //LISTS ALL PLAYERS IN THE DAT
 
 server.get("/leaderboardMongo", function (req, res) {
 
-    var leaderboard = player.find({ player_Score: { $exists: true } }).sort({ player_Score: -1 }).limit(10);
-    res.send({ leaderboard });
+    var leaderboard = player.find({ player_Score: { $exists: true } }).sort({ player_Score: -1 }).limit(10).stringify;
+
+    res.send({ leaderboard } );
 
     /*player.find(function (err, Player) {
         if (err) return console.error(err);
